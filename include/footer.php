6f5d054f2d3b123e1729d6a4bacbdfc0f3835180
Hristofor <147603156+Hristofor1234@users.noreply.github.com>
2024-06-22 13:11:04 +0500

    first commit

---
+++ include/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="flex">
+            <p>Веб-сайт <a href = "">web site</a></p>
+            <p>Телефон <a href = "">phone</a></p>
+            <p>Email <a href = "">Email</a></p>
+        </div>
+    </div>
+</footer>
---
+++ include/header.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="container">
+        <div class="flex">
+            <a href = "index.php">
+                <div class="img">
+                    <img src = "assets/img/home.png" alt = "">
+                </div>
+            </a>
+            <a href = "user.php">
+                <div class="img">
+                    <img src = "assets/img/user.png" alt = "">
+                </div>
+            </a>
+        </div>
+    </div>
+</header>
